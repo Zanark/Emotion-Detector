3f6bf13efc6634cebc1226544980511e2804884e
Zanark <kiodeba11@gmail.com>
2019-02-03 22:49:10 +0530

    created labels for images

--- faces_train.py
+++ faces_train.py
@@ -1,5 +1,16 @@
 import os
+from PIL import Image
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 IMAGE_DIR = os.path.join(BASE_DIR , "images")
 
+y_labels = []
+x_train = []
+
+for root, dirs, files in os.walk(IMAGE_DIR):
+    for file in files:
+        if file.endswith("png") or file.endswith("jpg"):
+            path = os.path.join(root, file)
+            label = os.path.basename(root).replace(" " , "-").lower()
+            print(label, path)
+            pil_image = Image.open(path)
