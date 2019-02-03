1e7f466fb9e3f03a62829174d47e2678cf44ed0e
Zanark <kiodeba11@gmail.com>
2019-02-03 22:01:38 +0530

    saving captured face

--- faces.py
+++ faces.py
@@ -1,16 +1,23 @@
 import numpy as np
 import cv2
 
-faceCascade = cv2.CascadeClassifier('cascades/data/haarcascade_frontalface_alt2.xml')
+FaceCascade = cv2.CascadeClassifier('cascades/data/haarcascade_frontalface_alt2.xml')
 
 cap = cv2.VideoCapture(0)
 
-while(True):
+while True:
     #Capture frame by frame,
     ret, frame = cap.read()
 
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    faces = FaceCascade.detectMultiScale(gray, scaleFactor=1.5, minNeighbors=5)
+    for(x, y, w, h) in faces:
+        print(x, y, w, h)
+        roi_gray = gray[y:y+h, x:x+w]
+        imgItem = "my-face.png"
+        cv2.imwrite(imgItem , roi_gray)
     #Display the frame
-    cv2.imshow('frame' , frame)
+    cv2.imshow('frame', frame)
     if cv2.waitKey(20) & 0xFF == ord('q'):
         break
 
